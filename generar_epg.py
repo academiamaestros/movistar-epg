7faa4e6d770920934c90fd9d601accd76c325957
academiamaestros <academiamaestra2025@gmail.com>
2025-03-12 20:13:31 +0100

    Update generar_epg.py

--- generar_epg.py
+++ generar_epg.py
@@ -1,7 +1,6 @@
 import json
 import requests
 import xml.etree.ElementTree as ET
-import datetime
 from datetime import datetime
 
 # Obtener la fecha y hora actual
@@ -9,10 +8,11 @@ fecha_actual = datetime.utcnow().strftime("%d/%m/%Y %H:%M")
 
 # Crear la cabecera del XML con la fecha
 cabecera = f'<?xml version="1.0" encoding="UTF-8"?>\n'
-cabecera += f'<tv generator-info-name="Epg Movistar {fecha_actual}" >\n'
+cabecera += f'<tv generator-info-name="Epg Movistar {fecha_actual}" generator-info-url="t.me/EPG_dobleM">\n'
 
-# Abre el archivo para escribir la cabecera y demás datos
-with open('guiaiptv.xml', 'w', encoding='utf-8') as
+# Abre el archivo para escribir la cabecera
+with open('guiaiptv.xml', 'w', encoding='utf-8') as archivo:
+    archivo.write(cabecera)
 
 # Descargar los datos JSON desde la URL
 url = 'https://ottcache.dof6.com/movistarplus/webplayer/DIFUSION/contents/epg'
@@ -29,9 +29,13 @@ for canal in programacion:
     display_name.text = canal["Nombre"]
 
     for pase in canal["Pases"]:
-        program = ET.SubElement(root, "programme", start=datetime.datetime.fromtimestamp(int(pase["FechaHoraInicio"])/1000).strftime('%Y%m%d%H%M%S') + " +0000",
-                                stop=datetime.datetime.fromtimestamp(int(pase["FechaHoraFin"])/1000).strftime('%Y%m%d%H%M%S') + " +0000",
-                                channel=canal["Nombre"])
+        program = ET.SubElement(
+            root, 
+            "programme", 
+            start=datetime.fromtimestamp(int(pase["FechaHoraInicio"]) / 1000).strftime('%Y%m%d%H%M%S') + " +0000",
+            stop=datetime.fromtimestamp(int(pase["FechaHoraFin"]) / 1000).strftime('%Y%m%d%H%M%S') + " +0000",
+            channel=canal["Nombre"]
+        )
         title = ET.SubElement(program, "title")
         title.text = pase["Titulo"]
         desc = ET.SubElement(program, "desc")
@@ -39,8 +43,12 @@ for canal in programacion:
         category = ET.SubElement(program, "category")
         category.text = pase["GeneroComAntena"]
 
-# Guardar el archivo XML
+# Guardar el resto del archivo XML
 tree = ET.ElementTree(root)
 tree.write("guiaiptv.xml", encoding='utf-8', xml_declaration=True)
+
+# Cerrar el XML correctamente
+with open('guiaiptv.xml', 'a', encoding='utf-8') as archivo:
+    archivo.write('</tv>\n')
 
 print("EPG generada correctamente.")
